380efcfc3c76f9bc36c5695f8a0a55c5f4b77550
abhishek <abhij89@gmail.com>
2023-07-14 13:47:42 +0530

    update plugin to manifest v3

--- popup.js
+++ popup.js
@@ -1,7 +1,23 @@
+async function getCurrentTab() {
+    let queryOptions = {active: true, currentWindow: true};
+    return await chrome.tabs.query(queryOptions)
+    .then((tabs) => {
+        console.log(tabs);
+        return tabs[0];
+    })
+    .catch((error) => {
+        console.log(error);
+        return;
+    })
+}
+document.addEventListener('DOMContentLoaded', async function() {
+    tab = await getCurrentTab();
+});
 function getMetas() {
 	var message = document.querySelector('#metaTable');
-	chrome.tabs.executeScript(null, {
-		file: "getPageMetas.js"
+	chrome.scripting.executeScript({
+		  target: {tabId: tab.id},
+          files: ['getPageMetas.js']
 	}, function() {
 		// If you try it into an extensions page or the webstore/NTP you'll get an error
 		if (chrome.runtime.lastError) {
@@ -13,9 +29,9 @@ function getMetas() {
 chrome.runtime.onMessage.addListener(function(request, sender) {
 	var metaTable = document.getElementById('metaTable');
 	if (request.method == "getMetas") {
-		for (var i=0; i<request.metas.length; i++) { 
-			metaTable.innerHTML += "<tr><td>"+request.metas[i][0]+"</td><td>"+request.metas[i][1]+"</td><td>"+request.metas[i][2]+"</td><td>"+request.metas[i][3]+"</td><td>"+request.metas[i][4]+"</td></tr>"; 
-		} 
+		for (var i=0; i<request.metas.length; i++) {
+			metaTable.innerHTML += "<tr><td>"+request.metas[i][0]+"</td><td>"+request.metas[i][1]+"</td><td>"+request.metas[i][2]+"</td><td>"+request.metas[i][3]+"</td><td>"+request.metas[i][4]+"</td></tr>";
+		}
 	}
 });
 
